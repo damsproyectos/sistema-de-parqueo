55c7d2bee4cb03a076ec1bee624832304d14bae1
damsproyectos <companydams@gmail.com>
2023-02-23 10:47:41 -0500

    Se agregó el módulo clientes finalizado

---
+++ clientes/controller_update.php
@@ -0,0 +1,34 @@
+
+<?php
+include ('../app/config.php');
+
+$nombre_cliente = $_GET['nombre_cliente'];
+$nit_ci_cliente = $_GET['nit_ci_cliente'];
+$placa_auto = $_GET['placa_auto'];
+$id_cliente = $_GET['id_cliente'];
+//echo $nombres."-".$email."-".$password_user;
+
+date_default_timezone_set("America/Bogota");
+$fechaHora= date("Y-m-d h:i:s");
+$sentencia = $pdo->prepare("UPDATE tb_clientes SET
+nombre_cliente = :nombre_cliente,
+nit_ci_cliente = :nit_ci_cliente,
+placa_auto = :placa_auto,
+fyh_actualizacion = :fyh_actualizacion
+WHERE id_cliente = :id_cliente");
+
+$sentencia->bindParam(':nombre_cliente', $nombre_cliente);
+$sentencia->bindParam(':nit_ci_cliente', $nit_ci_cliente);
+$sentencia->bindParam(':placa_auto', $placa_auto);
+$sentencia->bindParam(':fyh_actualizacion', $fechaHora);
+$sentencia->bindParam(':id_cliente', $id_cliente);
+
+if($sentencia->execute()) {
+    echo "se actualizo el registro de la manera correcta";
+    ?>
+    <script>location.href = "index.php";</script>
+    <?php
+}else{
+    echo "error al actualizar el registro";
+}
+?>
